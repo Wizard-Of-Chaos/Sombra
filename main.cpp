84c1b6d7a05fc027401be01cb76d599616b2d8af
Michael Eddins <michaeleddins4@knuth.cs.longwood.edu>
2020-11-09 16:59:59 -0500

    wrote like 2 lines of code that don't work

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+vector<vector<Point>> get_map(std::string filename)
+{
+	vector<vector<Point>> map;
+	ifstream grab(filename);
+	while(getline(grab))
+	{
+		//load stuff into the map vector
+	}
+	grab.close();
+	return map;
+}
 
 int main (int argc, char** argv) 
 {
