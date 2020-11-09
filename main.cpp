8cdc1bc02774674c62c51080a283b3931bfa0a6a
Alexander Wiecking <festningdal@amdahl.cs.longwood.edu>
2020-11-09 17:02:05 -0500

    Point struct

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 #include <string>
 
+struct Point
+{
+  int x, y;
+  Point(int x_entry, int y_entry) : x(x_entry), y(y_entry) {}
+};
+
 vector<vector<Point>> get_map(std::string filename)
 {
 	vector<vector<Point>> map;
